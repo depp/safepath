3d3b1d1db562e47dc2b493a1d4c85acfaed6e15c
Dietrich Epp <depp@zdome.net>
2021-04-15 14:37:12 -0400

    Add second version of safepath module
    This adds the Rules type, which describes different restrictions that
    you can apply to paths.

--- safepath.go
+++ safepath.go
@@ -5,43 +5,273 @@
 package safepath
 
 import (
-	"regexp"
+	"fmt"
+	"strconv"
 	"strings"
+	"unicode/utf8"
 )
 
-var (
-	safe   = regexp.MustCompile("^[A-Za-z0-9]+(?:[-_.][A-Za-z0-9]+)*$")
-	unsafe = regexp.MustCompile(
-		`^(?i)(?:con|prn|aux|nul|com[1-9]|lpt[1-9])(?:\.[^.]*)?$`)
+// A Rules is a set of restrictions for file names. Rules are bitflags, and
+// applying more bitflags applies stricter rules.
+type Rules uint8
+
+const (
+	// URLUnescaped requires that paths do not need hex escaping in URLs.
+	URLUnescaped Rules = 1 << iota
+	// ShellSafe requires that paths are safe for use in a POSIX shell.
+	ShellSafe
+	// WindowsSafe requires that paths are safe for Windows filesystems.
+	WindowsSafe
+	// NotHidden requires that paths do not start with a period.
+	NotHidden
+	// laxRules are rules that always apply.
+	laxRules
+	// Strict is the strictest set of rules.
+	Strict = URLUnescaped | ShellSafe | WindowsSafe | NotHidden
 )
 
-// IsSafeSegment returns true if the given string is a safe path segment.
-//
-// - Only contains alphanumeric characters and "-", ".", "_".
-// - Must begin and end with alphanumeric character.
-// - Cannot have two consecutive non-alphanumeric characters.
-// - Cannot be a forbidden filename on Windows.
-//
-// Forbidden filenames on Windows are con, prn, aux, nul, com[1-9], and
-// lpt[1-9].
-func IsSafeSegment(name string) bool {
-	return safe.MatchString(name) && !unsafe.MatchString(name)
+// GoString implements the GoStringer interface.
+func (r Rules) GoString() string {
+	var s []string
+	if r&URLUnescaped != 0 {
+		r &^= URLUnescaped
+		s = append(s, "URLUnescaped")
+	}
+	if r&ShellSafe != 0 {
+		r &^= ShellSafe
+		s = append(s, "ShellSafe")
+	}
+	if r&WindowsSafe != 0 {
+		r &^= WindowsSafe
+		s = append(s, "WindowsSafe")
+	}
+	if r&NotHidden != 0 {
+		r &^= NotHidden
+		s = append(s, "NotHidden")
+	}
+	if r == 0 {
+		if len(s) == 0 {
+			return "0"
+		}
+		return strings.Join(s, "|")
+	}
+	s = append(s, fmt.Sprintf("0x%02x", r))
+	return strings.Join(s, "|")
 }
 
-// IsSafePath returns true if the given path is a safe path. A safe path
-// consists of a non-empty list of safe segments separated by slashes.
-func IsSafePath(name string) bool {
-	for {
-		if name == "" {
-			return false
-		}
-		i := strings.IndexByte(name, '/')
-		if i == -1 {
-			return IsSafeSegment(name)
-		}
-		if !IsSafeSegment(name[:i]) {
-			return false
-		}
-		name = name[i+1:]
+var flags [128]Rules
+var windowsReserved map[string]bool
+
+func init() {
+	const strict = Strict | laxRules
+	for c := 1; c <= 127; c++ {
+		flags[c] = laxRules
 	}
+	// Alphanumerics are always safe.
+	for c := '0'; c <= '9'; c++ {
+		flags[c] = strict
+	}
+	for c := 'a'; c <= 'z'; c++ {
+		flags[c] = strict
+	}
+	for c := 'A'; c <= 'Z'; c++ {
+		flags[c] = strict
+	}
+	// Shell and Windows use blacklist, URLUnescaped uses whitelist.
+	for c := 33; c <= 126; c++ {
+		flags[c] |= ShellSafe | WindowsSafe
+	}
+	flags['/'] = 0
+
+	// RFC 3986 section 3.3
+	// https://tools.ietf.org/html/rfc3986#section-3.3
+	// pchar = unreserved / pct-encoded / sub-delims / ":" / "@"
+	// unreserved    = ALPHA / DIGIT / "-" / "." / "_" / "~"
+	// sub-delims    = "!" / "$" / "&" / "'" / "(" / ")"
+	//               / "*" / "+" / "," / ";" / "="
+	// Colon is disallowed in the first segment of a relative path, so we
+	// disallow it everywher.
+	for _, c := range "-._~!$&'()*+,;=" {
+		flags[c] |= URLUnescaped
+	}
+
+	// IEEE Std1003.1-2017 section C.2 Shell Command Language
+	// https://pubs.opengroup.org/onlinepubs/9699919799/xrat/V4_xcu_chap02.html
+	// Needs quoting: "|" / "&" / ";" / "<" / ">" / "(" / ")" / "$" / "`" / "\" / <"> / "'"
+	for _, c := range "|&;<>()$`\\\"'" {
+		flags[c] &^= ShellSafe
+	}
+
+	// MSDN: Naming Files, Paths, and Namespaces
+	// https://docs.microsoft.com/en-us/windows/win32/fileio/naming-a-file
+	// Needs quoting: "<" / ">" / ":" / """ / "/" / "\" / "|" / "?" / "*"
+	flags[' '] |= WindowsSafe
+	for _, c := range "<>:\"/\\|?*" {
+		flags[c] &^= WindowsSafe
+	}
+	windowsReserved = make(map[string]bool, 4+9*2)
+	for _, name := range []string{"con", "prn", "aux", "nul"} {
+		windowsReserved[name] = true
+	}
+	for _, name := range []string{"com", "lpt"} {
+		for i := 1; i <= 9; i++ {
+			windowsReserved[name+strconv.Itoa(i)] = true
+		}
+	}
+}
+
+const (
+	// Path segment errors.
+	errBad = iota
+	errFirst
+	errLast
+	errAny
+	errUnicode
+	errWReserved
+
+	// Path errors.
+	errEmpty
+	errAbsolute
+	errTrailingSlash
+	errDoubleSlash
+)
+
+type pathSegmentError struct {
+	isPath bool
+	path   string
+	name   string
+	err    int
+	char   rune
+	base   string
+}
+
+func (e *pathSegmentError) Error() string {
+	var msg string
+	switch e.err {
+	case errBad:
+	case errFirst:
+		msg = fmt.Sprintf("starts with disallowed character %q", e.char)
+	case errLast:
+		msg = fmt.Sprintf("ends with disallowed character %q", e.char)
+	case errAny:
+		msg = fmt.Sprintf("contains disallowed character %q", e.char)
+	case errUnicode:
+		msg = fmt.Sprintf("contains non-ASCII character %q U+%04X", e.char, e.char)
+	case errWReserved:
+		msg = fmt.Sprintf("uses reserved Windows filename %q", e.base)
+	case errEmpty:
+		msg = "path is empty"
+	case errAbsolute:
+		msg = "path is absolute"
+	case errTrailingSlash:
+		msg = "path has trailing slash"
+	case errDoubleSlash:
+		msg = "path has double slash"
+	default:
+		panic("invalid pathSegmentError")
+	}
+	if e.err <= errWReserved {
+		prefix := fmt.Sprintf("invalid path segment %q", e.name)
+		if msg != "" {
+			msg = prefix + ": " + msg
+		} else {
+			msg = prefix
+		}
+	}
+	if e.isPath {
+		prefix := fmt.Sprintf("invalid path %q", e.path)
+		if msg != "" {
+			msg = prefix + ": " + msg
+		} else {
+			msg = prefix
+		}
+	}
+	return msg
+}
+
+// CheckPathSegment returns an error if the given name is not a safe path
+// segment according to the chosen rules.
+//
+// The empty name, ".", and ".." are always unsafe, and '/' and is always an
+// unsafe character
+func (r Rules) CheckPathSegment(name string) error {
+	r = (r & Strict) | laxRules
+	if name == "" || name == "." || name == ".." {
+		return &pathSegmentError{name: name, err: errBad}
+	}
+	first, _ := utf8.DecodeRuneInString(name)
+	last, _ := utf8.DecodeLastRuneInString(name)
+	if r&ShellSafe != 0 {
+		if first == '-' || first == '~' {
+			return &pathSegmentError{name: name, err: errFirst, char: first}
+		}
+	}
+	if r&WindowsSafe != 0 {
+		if last == '.' || last == ' ' {
+			return &pathSegmentError{name: name, err: errLast, char: last}
+		}
+		i := strings.IndexByte(name, '.')
+		if i == -1 || strings.IndexByte(name[i+1:], '.') == -1 {
+			base := name
+			if i != -1 {
+				base = name[:i]
+			}
+			if i == 3 || i == 4 {
+				base = strings.ToLower(base)
+				if windowsReserved[base] {
+					return &pathSegmentError{name: name, err: errWReserved, base: base}
+				}
+			}
+		}
+	}
+	if r&NotHidden != 0 {
+		if first == '.' {
+			return &pathSegmentError{name: name, err: errFirst, char: first}
+		}
+	}
+	r &^= NotHidden
+	for _, c := range name {
+		if c >= 128 {
+			return &pathSegmentError{name: name, err: errUnicode, char: c}
+		}
+		f := flags[c]
+		if f&r != r {
+			return &pathSegmentError{name: name, err: errAny, char: c}
+		}
+	}
+	return nil
+}
+
+// CheckPath returns an error if the given path is not a safe path. A safe path
+// consists of a non-empty list of safe segments separated by slashes.
+func (r Rules) CheckPath(name string) error {
+	if len(name) == 0 {
+		return &pathSegmentError{isPath: true, path: name, err: errEmpty}
+	}
+	if name[0] == '/' {
+		return &pathSegmentError{isPath: true, path: name, err: errAbsolute}
+	}
+	for len(name) != 0 {
+		var part string
+		switch i := strings.IndexByte(name, '/'); {
+		case i == 0:
+			return &pathSegmentError{isPath: true, path: name, err: errDoubleSlash}
+		case i == -1:
+			part = name
+			name = ""
+		default:
+			part = name[:i]
+			name = name[i+1:]
+			if len(name) == 0 {
+				return &pathSegmentError{isPath: true, path: name, err: errTrailingSlash}
+			}
+		}
+		if err := r.CheckPathSegment(part); err != nil {
+			e := err.(*pathSegmentError)
+			e.isPath = true
+			e.path = name
+			return e
+		}
+	}
+	return nil
 }
--- safepath_test.go
+++ safepath_test.go
@@ -1,42 +1,80 @@
 package safepath
 
 import (
+	"strconv"
 	"testing"
 )
 
+func testResult(t *testing.T, expect bool, r Rules, name, input string, err error) {
+	t.Helper()
+	if !expect {
+		if err == nil {
+			t.Errorf("%#v.%s(%q) = nil, expect error", r, name, input)
+		}
+	} else {
+		if err != nil {
+			t.Errorf("%#v.%s(%q) = %v, expect ok", r, name, input, err)
+		}
+	}
+}
+
 func TestSafepath(t *testing.T) {
-	safeSeg := []string{
-		"a", "0123", "A", "file.txt", "My_Archive-v0.1.3.tar.gz",
-		"nul.txt.foo",
+	allRules := []Rules{URLUnescaped, ShellSafe, WindowsSafe, NotHidden, laxRules}
+	type testcase struct {
+		rules Rules
+		name  string
 	}
-	for _, s := range safeSeg {
-		if !IsSafeSegment(s) {
-			t.Errorf("IsSafeSegment(%q): false, expect true", s)
-		}
+	cases := []testcase{
+		{0, ""},
+		{0, "."},
+		{0, ".."},
+		{0, "\u0080"},
+		{Strict, "a"},
+		{Strict, "0123"},
+		{Strict, "A"},
+		{Strict, "file.txt"},
+		{Strict, "My_Archive-v0.1.3.tar.gz"},
+		{Strict, "nul.txt.foo"},
+		{Strict &^ ShellSafe, "~username"},
+		{Strict &^ WindowsSafe, "nul.txt"},
+		{Strict &^ ShellSafe, "$dollar$"},
+		{Strict &^ NotHidden, ".foo"},
+		{Strict &^ WindowsSafe, "a."},
+		{WindowsSafe | NotHidden, "a b"},
+		{NotHidden, "ab "},
+		{Strict &^ WindowsSafe, "ab."},
+		{Strict &^ ShellSafe, "-"},
+		{Strict &^ ShellSafe, "--abc"},
 	}
-	unsafeSeg := []string{
-		".foo", "a..b", "a__b", "a.", "a bc", ".", "-", "_",
-		"nul", "com1", "com1.txt", "nul.html",
+	for _, c := range cases {
+		c := c
+		t.Run(strconv.Quote(c.name), func(t *testing.T) {
+			cr := c.rules
+			if cr != 0 {
+				cr |= laxRules
+			}
+			for _, r := range allRules {
+				expect := r&cr == r
+				testResult(t, expect, r, "CheckPathSegment", c.name, r.CheckPathSegment(c.name))
+				testResult(t, expect, r, "CheckPath", c.name, r.CheckPath(c.name))
+				long := c.name + "/" + c.name + "/" + c.name
+				testResult(t, expect, r, "CheckPath", long, r.CheckPath(long))
+			}
+		})
 	}
-	for _, s := range unsafeSeg {
-		if IsSafeSegment(s) {
-			t.Errorf("IsSafeSegment(%q): true, expect false", s)
-		}
+	pcases := []string{
+		"",
+		"/",
+		"abc//def",
+		"/abc/def",
+		"abc/def/",
 	}
-	safePath := []string{
-		"abc", "abc/def", "abc.dir/def.txt", "1/2/3/4/5/6_7_8",
-	}
-	for _, s := range safePath {
-		if !IsSafePath(s) {
-			t.Errorf("IsSafePath(%q): false, expect true", s)
-		}
-	}
-	unsafePath := []string{
-		".", "/", "abc/",
-	}
-	for _, s := range unsafePath {
-		if IsSafePath(s) {
-			t.Errorf("IsSafePath(%q): true, expect false", s)
-		}
+	for _, c := range pcases {
+		c := c
+		t.Run(strconv.Quote(c), func(t *testing.T) {
+			if err := laxRules.CheckPath(c); err == nil {
+				t.Errorf("CheckPath(%q) = nil, expect error", c)
+			}
+		})
 	}
 }
